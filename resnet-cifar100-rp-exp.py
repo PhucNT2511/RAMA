d0ebbdb738a1efaec7e2e859e851c96d22cdab5c
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-01-23 00:47:02 +0700

    saved

--- resnet-cifar100-rp-exp.py
+++ resnet-cifar100-rp-exp.py
@@ -266,6 +266,11 @@ def main(args: argparse.Namespace) -> None:
                 "Test/Loss":val_loss,
                 "Test/Acc":val_acc,
                 })
+        if args.use_rp:
+            logger.info(f"Lambda Value: {model.rp.lambda_param.item()}")
+            wandb.log({
+                "Lambda":model.rp.lambda_param.item(),
+            })
 
     logger.info(f"Training finished. Best validation accuracy: {best_acc:.2f}%")
     writer.close()
