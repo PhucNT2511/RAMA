9a9edd8a579ef30b20f52b98a938a74abacdc0ae
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-01-23 10:32:40 +0700

    saved

--- resnet-cifar100-rp-exp.py
+++ resnet-cifar100-rp-exp.py
@@ -260,7 +260,7 @@ def main(args: argparse.Namespace) -> None:
                     f"Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.2f}%")
         
         wandb.log({
-                "epoch": epoch,
+                "step": epoch,
                 "Train/Loss": train_loss,
                 "Train/Acc": train_acc,
                 "Test/Loss":val_loss,
@@ -269,7 +269,7 @@ def main(args: argparse.Namespace) -> None:
         if args.use_rp:
             logger.info(f"Lambda Value: {model.rp.lambda_param.item()}")
             wandb.log({
-                "epoch": epoch,
+                "step": epoch,
                 "Lambda":model.rp.lambda_param.item(),
             })
 
