34f91c1d23ee4182170088c981459f3388554964
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-02-08 00:32:34 +0700

    saved

--- image-classification-rp-exps.py
+++ image-classification-rp-exps.py
@@ -93,6 +93,7 @@ class DatasetManager:
             transform = transforms.Compose([
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
+                transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ])
