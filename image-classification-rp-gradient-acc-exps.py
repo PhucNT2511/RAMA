b8a63cc98f0c7923e19ad0a7f0cdf7f24a7abb6e
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-02-16 22:19:04 +0700

    saved

--- image-classification-rp-gradient-acc-exps.py
+++ image-classification-rp-gradient-acc-exps.py
@@ -90,7 +90,7 @@ class DatasetManager:
             transform = transforms.Compose([
                 transforms.Resize((224, 224)),
                 transforms.ToTensor(),
-                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+                transforms.Normalize(mean=[0.5], std=[0.5]),
             ])
 
             train_dataset = torchvision.datasets.Omniglot(root="./data", background=True, download=True,
