c76a80f33457c158bcfb787e35efcd9b0b96170d
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-03-14 00:07:48 +0700

    saved

--- image-classification-rp-gradient-acc-exps.py
+++ image-classification-rp-gradient-acc-exps.py
@@ -170,7 +170,7 @@ class RanPACLayer(nn.Module):
         '''
         # Break graph
         if self.clamp:
-            self.lambda_param.data.clamp_(0.0001, 0.005)
+            self.lambda_param.data.clamp_(0.0001, 0.2)
 
         x = self.projection(x) * self.lambda_param  * self.sqrt_d
         if self.non_linearities == 'leaky_relu':
