f914e52155fd5b822c67d6ba4c842ca6a767e957
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-03-16 02:50:21 +0700

    saved

--- image-classification-rp-gradient-acc-exps.py
+++ image-classification-rp-gradient-acc-exps.py
@@ -144,7 +144,7 @@ class RanPACLayer(nn.Module):
             self.clamp = False
         else:
             self.sqrt_d = math.sqrt(input_dim)
-            self.lambda_param = nn.Parameter(torch.tensor(0.02))  ########
+            self.lambda_param = nn.Parameter(torch.tensor(0.001))  ########
             self.clamp = True
         self.norm = nn.BatchNorm1d(output_dim) if norm_type == "batch" else nn.LayerNorm(output_dim)
         self.non_linearities = non_linearities
@@ -170,7 +170,7 @@ class RanPACLayer(nn.Module):
         '''
         # Break graph
         if self.clamp:
-            self.lambda_param.data.clamp_(0, 0.5)
+            self.lambda_param.data.clamp_(0, 0.01)
 
         x = self.projection(x) * self.lambda_param  * self.sqrt_d
         if self.non_linearities == 'leaky_relu':
