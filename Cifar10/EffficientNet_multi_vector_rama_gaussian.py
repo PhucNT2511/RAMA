779585b9a3a91664e956ff422eb367f98020b6df
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-05-01 01:09:38 +0700

    saved

--- Cifar10/EffficientNet_multi_vector_rama_gaussian.py
+++ Cifar10/EffficientNet_multi_vector_rama_gaussian.py
@@ -737,7 +737,7 @@ def parse_args():
     # Gaussian RAMA configuration
     parser.add_argument('--use-rama', action='store_true', help='whether to use RAMA layers')
     parser.add_argument('--use-hyperparameter-optimization', action='store_true', help='whether to use Bayesian optimization for p-value')
-    parser.add_argument('--lambda-value', default=1.0, type=float, help='Lambda_value for RAMA')
+    parser.add_argument('--lambda-value', default=0.05, type=float, help='Lambda_value for RAMA')
     parser.add_argument('--sqrt-dim', default= False, help='Whether multiply with sqrt(d) or not')
     parser.add_argument('--use-normalization', action='store_true', help='use layer normalization in RAMA layers')
     parser.add_argument('--activation', default='relu', choices=['relu', 'leaky_relu', 'tanh', 'sigmoid'],
