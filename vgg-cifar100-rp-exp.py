6e38f04fa7e9a2e6bca1156007e411cd97b2d660
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-05-08 12:28:07 +0700

    saved

--- vgg-cifar100-rp-exp.py
+++ vgg-cifar100-rp-exp.py
@@ -262,15 +262,15 @@ def main(args: argparse.Namespace) -> None:
 
     # Transforms & Dataset
     transform_train = transforms.Compose([
-        transforms.Resize(224),
-        transforms.RandomCrop(224, padding=16), 
+#        transforms.Resize(224),
+        transforms.RandomCrop(32, padding=4), 
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
     ])
 
     transform_test = transforms.Compose([
-        transforms.Resize(224),
+#        transforms.Resize(224),
         transforms.ToTensor(),
         transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
     ])
