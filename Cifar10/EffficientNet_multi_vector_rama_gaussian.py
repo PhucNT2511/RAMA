f47dbb7166394af128cfa48206e5ec9f2bf3beba
Nguyen Thang Phuc <phuca1tt1bn@gmail.com>
2025-05-16 09:54:22 +0700

    saved

--- Cifar10/EffficientNet_multi_vector_rama_gaussian.py
+++ Cifar10/EffficientNet_multi_vector_rama_gaussian.py
@@ -498,7 +498,7 @@ class Trainer:
                 attack_model_wrapper = AttackModelWrapper(self.model, current_lambda_for_eval).to(self.device)
                 
 
-                if (test_acc > self.best_acc and epoch % 15 == 0) or (epoch == total_epochs - 1):
+                if  epoch % 15 == 0 or epoch == total_epochs - 1:
                     with torch.enable_grad():
                         # FGSM Attack Evaluation
                         if self.args and self.args.eval_fgsm:
@@ -914,6 +914,8 @@ def main():
         use_rama=args.use_rama,
         rama_config=rama_config
     ).to(device)
+
+
 
     # Loss function and optimizer
     criterion = nn.CrossEntropyLoss()
